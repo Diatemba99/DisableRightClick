60d38f70463d6bd57f4eef5e1e70194b6a039d39
Martin Claude Diatta <58808297+Diatemba99@users.noreply.github.com>
2020-04-16 23:52:29 +0000

    Add files via upload

---
+++ tuto2_disableRightClick/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function()
+{
+    $('body').bind('cut copy paste', function (e) 
+       {
+         e.preventDefault();
+       })
+    $("body").on("contextmenu", function (e)
+        {
+         return false;
+        })
+})
+
